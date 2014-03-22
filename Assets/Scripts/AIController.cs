a9ac13c6453ed76db90b3365f68628b1d2da469b
horatiu665 <croman12@student.aau.dk>
2014-03-22 13:55:37 +0100

    added NavMesh and AI controllers (pro only)

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+	public float rotationSpeed;
+	public float movementSpeed;
+	public Transform target;
+
+
+	private NavMeshAgent agent;
+	private Animator anim;
+
+	// the old position
+	private Vector3 oldPos;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.updateRotation = true;
+		anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float speed = agent.velocity.magnitude / agent.speed;
+		anim.SetFloat("Speed", speed);
+		agent.SetDestination(target.position);
+
+		
+	}
+
+}
