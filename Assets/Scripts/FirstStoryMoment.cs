45eb252cc3a6f8c134d9d06114cda4890dd9bf78
horatiu665 <croman12@student.aau.dk>
2014-03-23 00:57:41 +0100

    lots of stuff like

---
+++ Assets/Scripts/FirstStoryMoment.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstStoryMoment : MonoBehaviour
+{
+
+	public float startGameTime;
+
+	public Transform player;
+	public Camera cinematicCamera;
+
+	// Use this for initialization
+	void Start()
+	{
+		Invoke("StartGame", startGameTime);
+
+	}
+
+	void StartGame()
+	{
+		cinematicCamera.gameObject.SetActive(false);
+		player.gameObject.SetActive(true);
+
+
+	}
+
+}
